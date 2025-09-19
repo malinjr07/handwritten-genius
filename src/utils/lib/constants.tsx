64dff93a133a7ab5308ad4ebe3b3e2d59c00dd57
marufC9 <Mosaddik.AliMaruf@c9.com.au>
2025-09-19 14:43:18 +0600

    feat: Project clean up

--- src/utils/lib/constants.tsx
+++ src/utils/lib/constants.tsx
@@ -1,37 +1,37 @@
-import { tNavMenu } from "../types";
+import { tNavMenu } from '@customTypes/index';
 
 export const navMenu: tNavMenu[] = [
   {
-    title: "business",
-    url: "/",
+    title: 'business',
+    url: '/',
     subMenu: [
-      { title: "business overview", url: "/" },
+      { title: 'business overview', url: '/' },
       {
-        title: "market solution",
-        url: "/",
+        title: 'market solution',
+        url: '/',
         subMenu: [
-          { title: "automotive dealership", url: "/" },
-          { title: "insurance agent", url: "/" },
-          { title: "Non-Profit Organization", url: "/" },
+          { title: 'automotive dealership', url: '/' },
+          { title: 'insurance agent', url: '/' },
+          { title: 'Non-Profit Organization', url: '/' },
         ],
       },
     ],
   },
-  { title: "features", url: "/" },
+  { title: 'features', url: '/' },
   {
-    title: "integrations",
-    url: "/",
+    title: 'integrations',
+    url: '/',
     subMenu: [
-      { title: "integrations overview", url: "/" },
-      { title: "api documentation", url: "/" },
-      { title: "api reference", url: "/" },
-      { title: "hubspot", url: "/" },
-      { title: "salesforce", url: "/" },
-      { title: "zapier", url: "/" },
-      { title: "shopify", url: "/" },
+      { title: 'integrations overview', url: '/' },
+      { title: 'api documentation', url: '/' },
+      { title: 'api reference', url: '/' },
+      { title: 'hubspot', url: '/' },
+      { title: 'salesforce', url: '/' },
+      { title: 'zapier', url: '/' },
+      { title: 'shopify', url: '/' },
     ],
   },
-  { title: "pricing", url: "/" },
-  { title: "resources", url: "/" },
-  { title: "about", url: "/" },
+  { title: 'pricing', url: '/' },
+  { title: 'resources', url: '/' },
+  { title: 'about', url: '/' },
 ];
